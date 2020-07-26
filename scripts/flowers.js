90db7004fd052dd141692504fc54495d029802f5
The Muslim Garden <67616393+TheMuslimGarden@users.noreply.github.com>
2020-07-26 17:45:36 -0400

    Update flowers.js

--- scripts/flowers.js
+++ scripts/flowers.js
@@ -36,7 +36,7 @@ var drawAllTheFlowers = function() {
     frameCount++;
     if (frameCount >= 100) {
         if (flowers.length < 30) {
-            flowers.push(new flower(Math.random() * 100 + "%",Math.random() * 1000,10 + "%",Math.random() * 360,Math.random() * 3))
+            flowers.push(new flower(Math.random() * 100 + "%",Math.random() * 1000,10 + "%",Math.random() * 360,Math.random() * 3));
         }
         frameCount = 0;
     }
@@ -50,3 +50,35 @@ var drawAllTheFlowers = function() {
     window.requestAnimationFrame(drawAllTheFlowers);
 };
 drawAllTheFlowers();
+
+/*
+var Omar = {};
+
+Omar.prototype = function (Hair, Grade,Funny) {
+    this.Hair = Hair;
+    this.Grade= Grade;
+    this.Funny= Funny;
+    this.CreateWebsites = function () {
+        window.alert(this.Grade + " grade website is created");
+    };
+};
+int v = 9;
+Omar.ShoesSize = "9";
+
+var Abdurrahman = {
+    Hair: "Short",
+    Grade: "8th",
+    Funny: true
+};
+
+function PrintHair(X) {
+    window.alert(X.Hair)
+};
+
+var OmarObject = new Omar("Medium", "8th", true);
+
+fn(OmarObject);
+
+OmarObject.CreateWebsites();
+
+*/
