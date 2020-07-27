0e77a19ff1d7f49a37de22ee929f9e04ff06f20b
The Muslim Garden <67616393+TheMuslimGarden@users.noreply.github.com>
2020-07-27 17:16:19 -0400

    Update preview.js

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,10 +1,10 @@
-var d=document;
-    var Content=d.getElementById("content");
-    var previewBox=function(ImgURL,Title,Description,URL){
-        return "<a target='_blank' href='"+URL+"'><div class='videoBox'>    <img src='"+ImgURL+"' class='image'>    <h4>"+ Title+"</h4><p> "+Description+"</p></div></a>";
-    };
-    function drawBoxes(array){
-    for(var i=0; i<array.length; i++){
-    Content.innerHTML+=previewBox(array[i].imgSrc,array[i].name,array[i].description,array[i].URL);
+var d = document;
+var Content = d.getElementById("content");
+var previewBox = function(ImgURL, Title, Description, URL) {
+    return "<a target='_blank' href='" + URL + "'><div class='videoBox'>    <img src='" + ImgURL + "' class='image'>    <h4>" + Title + "</h4><p> " + Description + "</p></div></a>";
+};
+function drawBoxes(array) {
+    for (var i = 0; i < array.length; i++) {
+        Content.innerHTML += previewBox(array[i].imgSrc, array[i].name, array[i].description, array[i].URL);
     }
-    }
+}
